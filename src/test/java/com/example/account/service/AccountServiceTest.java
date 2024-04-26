0798bb40fc40d1126759a37168b4888677c5ed0c
soohomode <soohomode@gmail.com>
2024-04-27 00:22:54 +0900

    240427 test

---
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.account.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceTest {
+
+    @Test
+    void testSomething() {
+        String something = "Hello " + "World";
+
+        assertEquals("Hello World", something);
+    }
+
+    @Test
+    void testSomething2() {
+        String something = "Hello " + "World";
+
+        assertEquals("Hello World", something);
+    }
+
+}
